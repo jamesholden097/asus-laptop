e0052772bd23e8503b49dec9cdf622852f2e8e6e
jamesholden097 <jamesholden097@gmail.com>
2022-11-09 07:08:05 +0600

    "1667956085"

--- git_push.sh
+++ git_push.sh
@@ -5,5 +5,5 @@ GIT=`which git`
 ${GIT} add .
 commit_message='"'$(date +%s)'"'
 ${GIT} commit -m "${commit_message}"
-${GIT} push https://${GIT_USER}:${GIT_TOKEN}git@github.com:jamesholden097/asus-laptop.git HEAD
+${GIT} push https://${GIT_USER}:${GIT_TOKEN}@github.com/jamesholden097/asus-laptop
 #bash -c "${GIT} config --global user.name"
