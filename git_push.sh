63cd630f66f6339e5fe7c2ea5b14aa5f54fd9971
jamesholden097 <jamesholden097@gmail.com>
2022-11-09 06:03:28 +0600

    "1667952208"

--- git_push.sh
+++ git_push.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-bash -c "git add ."
-commit_message='"'$(date +%s)'- server cfg updated"'
-bash -c "git commit -m ${commit_message}"
-bash -c "git push"
+GIT=`which git`
+${GIT} add .
+commit_message='"'$(date +%s)'"'
+${GIT} commit -m "${commit_message}"
+${GIT} push
+
